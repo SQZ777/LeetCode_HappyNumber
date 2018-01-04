fb68f4a980ca1ae4fb85ec655514a332959c8a09
sqz777In91 <sqz777isme@gmail.com>
2018-01-04 15:08:01 +0800

    加入專案檔案。

---
+++ LeetCode_HappyNumber/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_HappyNumber
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
